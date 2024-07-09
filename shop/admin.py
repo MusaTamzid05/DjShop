e6a6e73bedf1c9a3f441c2dd495d3fa6d57daf82
musa <tamzid101@gmail.com>
2024-07-09 21:07:18 +0600

    Model product and Category

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Category
+from .models import Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug"]
+    perpulated_fields = {"slug" : ("name",)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+            "name", 
+            "slug",
+            "price",
+            "available",
+            "created",
+            "updated"
+            ]
+
+    list_filters = ["available", "created", "updated"]
+    list_editable = ["price", "available"]
+    perpulated_fields = {"slug" : ("name",)}
